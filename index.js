624489e089c2c0138e4cce867ad62a23d6228192
JoaoLucasWolf <joaolwolf@gmail.com>
2024-02-16 16:49:22 -0300

    aula sobre arrays e funcoes

---
+++ index.js
@@ -0,0 +1,100 @@
+// silence is golden
+
+
+/*
+var nome = prompt("escreva seu nome");
+var cor = prompt("escreva sua cor preferida");
+
+var frase = "A cor favorita de " + nome + " é " + cor + "."
+
+console.log(frase);
+*/
+
+
+/*
+        exercicio 1
+var frase2 = prompt("escreva uma frase");
+    console.log(frase2.toLocaleUpperCase());
+    console.log(frase2.replaceAll("o", "i"));
+    console.log(frase2.length);*/
+
+
+
+/*
+const listaDeCompras = ["Abacate", "Banana", "Tomate"]
+const segundoItem = listaDeCompras[2] //Tomate
+*/
+
+
+/*
+        exercicio 2
+var listaDeCachorros = ["Border Collie","Rottweiler","Pastor Alemão","Dogue Alemão","Golden Retriever"];
+var escolha = prompt("escolha um numero de 0 a 4");
+
+    console.log(listaDeCachorros[escolha]);
+*/
+
+
+/*
+            exercicio 3
+ var array = ["a", "b", "c", "d", "e", "f", "g", "h"];
+//            [0,   1,   2,   3,   4,   5,   6,   7];
+
+    array.splice(3,1);
+    console.log(array);
+*/
+
+//exercicio 4 
+
+/*
+const array = ["0", "1", "2", "3", "4", "5", "6"];
+
+    console.log(array.length)
+    console.log(array.push("7"));
+    console.log(array.splice(3,2));
+    console.log(array);
+    */
+
+
+/*    
+    const altura = 2 ;
+    const largura = 3;
+    const area = altura * largura;
+    console.log(area); //6 
+*/
+
+
+/*
+function calculaArea(altura, largura){
+    const area = altura * largura 
+    console.log(area)
+    }
+    calculaArea(2,3)
+*/
+
+/*
+function impreimirOlaMundo(){
+        console.log("Ola Mundo");
+}
+    impreimirOlaMundo(); // <--função sendo chamada 
+*/
+
+/*
+function funcaoNome(nome){
+    
+    console.log(`Ola ${nome}`);
+    }
+    
+funcaoNome("nome"); // <--função sendo chamada
+funcaoNome("joao");
+funcaoNome("Ana");
+funcaoNome("Gabriel");
+*/
+
+
+
+
+
+
+
+
